af7478cc0d1a048a2759fe890ea3ca4e0ec49610
mateusValentim6D76 <mateus.valentim19@hotmail.com>
2022-01-24 21:43:42 -0300

    Configurando internacionalização

---
+++ src/main/java/br/com/keeggo/customersservice/config/InternacionalizacaoConfig.java
@@ -0,0 +1,32 @@
+package br.com.keeggo.customersservice.config;
+
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+@Configuration
+public class InternacionalizacaoConfig {
+
+	//Define o arquivo de mensagem
+	@Bean
+	public MessageSource messageSource() {
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		//nome do arquivo que se encontra dentro de src/main/resources
+		messageSource.setBasename("messages");
+		//reconhece os caracteres especiais utilizado no nosso idioma
+		messageSource.setDefaultEncoding("ISO-8859-1");
+		messageSource.setDefaultLocale(Locale.getDefault());
+		return messageSource;
+	}
+	
+	@Bean
+	public LocalValidatorFactoryBean validatorFactoryBean() {
+		LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+		bean.setValidationMessageSource(messageSource());
+		return bean;
+	}
+}
--- src/main/java/br/com/keeggo/customersservice/rest/CustomerController.java
+++ src/main/java/br/com/keeggo/customersservice/rest/CustomerController.java
@@ -1,4 +1,4 @@
-package br.com.keeggo.customersservice.controller;
+package br.com.keeggo.customersservice.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
