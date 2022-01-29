e27f2492876e4b0e9fff51b2b79a42edb6b2afd6
mateusValentim6D76 <mateus.valentim19@hotmail.com>
2022-01-29 07:26:12 -0300

    Implementando método de pesquisa de Serviços Prestados

--- src/main/java/br/com/keeggo/customersservice/model/repository/ServiceProvidedRepository.java
+++ src/main/java/br/com/keeggo/customersservice/model/repository/ServiceProvidedRepository.java
@@ -1,10 +1,17 @@
 package br.com.keeggo.customersservice.model.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import br.com.keeggo.customersservice.model.entity.Customer;
 import br.com.keeggo.customersservice.model.entity.ServiceProvided;
 
 public interface ServiceProvidedRepository extends JpaRepository<ServiceProvided, Integer>{
+
+	@Query("select s from ServiceProvided s join s.customer c where upper (c.name) like upper (:name) and MONTH(s.dateService) = :month")
+	List<ServiceProvided> findByNameAndMonth(@Param("name") String name, @Param("month") Integer month);
 
 }
--- src/main/java/br/com/keeggo/customersservice/rest/ServiceProvidedController.java
+++ src/main/java/br/com/keeggo/customersservice/rest/ServiceProvidedController.java
@@ -2,12 +2,15 @@ package br.com.keeggo.customersservice.rest;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 import org.springframework.data.repository.support.Repositories;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -25,32 +28,35 @@ import lombok.RequiredArgsConstructor;
 //Cria um construtor com os argumentos obrigatórios
 @RequiredArgsConstructor
 public class ServiceProvidedController {
-	
+
 	private final CustomerRepository customerRepository;
 	private final ServiceProvidedRepository repository;
 	private final BigDecimalConverter bigDecimalConverter;
 
-
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public ServiceProvided saveService(@RequestBody ServiceProvidedDTO dto) {
-		//Realizando o parse da data do serviço -> para String
+		// Realizando o parse da data do serviço -> para String
 		LocalDate date = LocalDate.parse(dto.getDateService(), DateTimeFormatter.ofPattern("dd/MM/yyyy"));
 		Integer idCustomer = dto.getIdCustomer();
-		
-		Customer customer = customerRepository
-							.findById(idCustomer)
-							.orElseThrow(() -> 
-							new ResponseStatusException(
-									HttpStatus.BAD_REQUEST, "non-existent customer"));
-		
+
+		Customer customer = customerRepository.findById(idCustomer)
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "non-existent customer"));
+
 		ServiceProvided serviceProvided = new ServiceProvided();
 		serviceProvided.setDescription(dto.getDescription());
 		serviceProvided.setDateService(date);
 		serviceProvided.setCustomer(customer);
 		serviceProvided.setValue(bigDecimalConverter.converter(dto.getValue()));
-		
+
 		return repository.save(serviceProvided);
 	}
-	
+
+	@GetMapping
+	public List<ServiceProvided> search(@RequestParam(value = "name", required = false, defaultValue = "") String name,
+			@RequestParam(value = "month", required = false) Integer month) {
+		
+		return repository.findByNameAndMonth("%" + name + "%", month);
+	}
+
 }
